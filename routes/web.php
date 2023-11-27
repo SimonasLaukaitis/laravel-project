71fa0ba9268ac188c2b262fdb43e9a5a6937bf37
SimonasLaukaitis <91092011+SimonasLaukaitis@users.noreply.github.com>
2023-11-27 20:30:13 +0200

    first task begining

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,68 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Globalus duomenų masyvas su straipsnių informacija
+$posts = [
+    1 => [
+        'title' => 'First article',
+        'content' => 'First article content',
+        'is_new' => false,
+        'authors' => [
+            1 => [
+                'name' => 'Authorname_1',
+                'surname' => 'Authorsurname_1'
+            ],
+            2 => [
+                'name' => 'Authorname_2',
+                'surname' => 'Authorsurname_2'
+            ]
+        ]
+    ],
+    2 => [
+        'title' => 'Second article',
+        'content' => 'Second article content',
+        'is_new' => true,
+        'authors' => [
+            1 => [
+                'name' => 'Authorname_3',
+                'surname' => 'Authorsurname_3'
+            ],
+            2 => [
+                'name' => 'Authorname_4',
+                'surname' => 'Authorsurname_4'
+            ]
+        ]
+    ],
+    3 => [
+        'title' => 'Third article',
+        'content' => 'Third article content',
+        'is_new' => false,
+        'authors' => [
+            1 => [
+                'name' => 'Authorname_5',
+                'surname' => 'Authorsurname_5'
+            ],
+            2 => [
+                'name' => 'Authorname_6',
+                'surname' => 'Authorsurname_6'
+            ]
+        ]
+    ]
+];
+
+// 1 uzduotis
+
+Route::get('/posts', function () use ($posts) {
+    $response = '';
+
+    foreach ($posts as $post) {
+        $response .= "<h3>{$post['title']}</h3><p>{$post['content']}</p>";
+    }
+
+    return $response;
+})->name('posts.index');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
