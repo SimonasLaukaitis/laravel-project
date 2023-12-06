2b42b9168b9b0226e559915d3a2ffc8ec02afd0f
SimonasLaukaitis <91092011+SimonasLaukaitis@users.noreply.github.com>
2023-12-06 18:24:09 +0200

    a tags addded

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,9 @@
 <div class="container d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
     <h1 class="main-heading mb-5">{{ __('app.conferences')}}</h1>
     <div class="d-flex justify-content-center">
-        <button class="btn btn-primary mx-2">{{ __('app.client')}}</button>
-        <button class="btn btn-primary mx-2">{{ __('app.worker')}}</button>
-        <button class="btn btn-primary mx-2">{{ __('app.administrator')}}</button>
+       <a class="btn btn-primary mx-2" href="">{{ __('app.client')}}</a>
+       <a class="btn btn-primary mx-2" href="">{{ __('app.worker')}}</a>
+       <a class="btn btn-primary mx-2" href="">{{ __('app.administrator')}}</a>
     </div>
 </div>
 
